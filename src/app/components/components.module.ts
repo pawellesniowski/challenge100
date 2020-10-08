0850a34fe8507579f9f094c03af5b3aa198d6ff1
Pawel_Lesniowski <pawel.html@gmail.com>
2020-10-08 12:32:54 +0200

    day3: add accordion-component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { CardComponent } from './card/card.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { AccordionItemComponent } from './accordion/accordion-item/accordion-item.component';
 
 @NgModule({
-  declarations: [ToolbarComponent, CardComponent],
+  declarations: [ToolbarComponent, CardComponent, AccordionComponent, AccordionItemComponent],
   imports: [CommonModule],
-  exports: [ToolbarComponent, CardComponent],
+  exports: [ToolbarComponent, CardComponent, AccordionComponent],
 })
 export class ComponentsModule {}
