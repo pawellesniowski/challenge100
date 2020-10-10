2bb62746a25f868c892b1e957a4210519a6b886f
Pawel_Lesniowski <pawel.html@gmail.com>
2020-10-10 21:53:25 +0200

    day5/100: add fontawesome, star-rating-component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,9 +5,10 @@ import { CardComponent } from './card/card.component';
 import { AccordionComponent } from './accordion/accordion.component';
 import { AccordionItemComponent } from './accordion/accordion-item/accordion-item.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { StarRatingComponent } from './star-rating/star-rating.component';
 
 @NgModule({
-  declarations: [ToolbarComponent, CardComponent, AccordionComponent, AccordionItemComponent, ProgressBarComponent],
+  declarations: [ToolbarComponent, CardComponent, AccordionComponent, AccordionItemComponent, ProgressBarComponent, StarRatingComponent],
   imports: [CommonModule],
   exports: [ToolbarComponent, CardComponent, AccordionComponent, ProgressBarComponent],
 })
