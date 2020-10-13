7ce3e8af06da5b856221f243ea8fda2d82fea5dd
Pawel_Lesniowski <pawel.html@gmail.com>
2020-10-13 12:49:28 +0200

    task5/day8/100: star rating component logic working.

--- src/app/components/star-rating/star-rating.component.ts
+++ src/app/components/star-rating/star-rating.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-star-rating',
   templateUrl: './star-rating.component.html',
-  styleUrls: ['./star-rating.component.scss']
+  styleUrls: ['./star-rating.component.scss'],
 })
 export class StarRatingComponent implements OnInit {
+  @Input() rate = 5;
+  rateStars: number[];
+  starsLimit = 5;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.rateStars = this.convertToRateStars(this.rate);
   }
 
+  convertToRateStars(
+    numberOfStars,
+    arrayOfStars = []
+  ): number[] {
+    if (arrayOfStars.length === this.starsLimit) {
+      return arrayOfStars;
+    }
+
+    if (numberOfStars >= 1) {
+      arrayOfStars.push(1);
+      return this.convertToRateStars(
+        numberOfStars - 1,
+        arrayOfStars
+      );
+    }
+
+    if (numberOfStars > 0) {
+      arrayOfStars.push(numberOfStars);
+      return this.convertToRateStars(
+        numberOfStars - numberOfStars,
+        arrayOfStars
+      );
+    }
+
+    arrayOfStars.push(numberOfStars);
+    return this.convertToRateStars(
+      numberOfStars,
+      arrayOfStars
+    );
+  }
 }
