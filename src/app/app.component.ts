74db980801a1d3e96ccd863e94c921b8717cacc7
Pawel_Lesniowski <pawel.html@gmail.com>
2020-10-21 21:04:34 +0200

    day16/task7/100: add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'challenge100';
-  public accordionItems = [
-    { title: 'Example 1', content: 'Example content 1', isExtended: false },
-    { title: 'Example 2', content: 'Example content 2', isExtended: false },
-  ];
 }
