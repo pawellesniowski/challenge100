097414ddddff6ab6fbcf54d23ebc34a1f1a16f0d
pawellesniowski <pawel.html>
2021-03-18 14:31:59 +0100

    Flatten Arr Pipe

---
+++ src/app/pipes/flatten-arr.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FlattenArrPipe } from './flatten-arr.pipe';
+
+describe('FlattenArrPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FlattenArrPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
